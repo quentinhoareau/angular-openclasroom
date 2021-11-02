52c4ec46bf6896fb0f9df6861bb46b3a944627c9
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2021-11-02 16:41:37 +0100

    nav no finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,41 +6,8 @@ import { AppareilService } from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  isAuth = false;
-  lastUpdate =  new Date();
-  // lastUpdate = new Promise(
-  //   (resolve, reject) => {
-  //     const date = new Date();
-  //     setTimeout(
-  //       () => {
-  //         resolve(date);
-  //       }, 2000
-  //     )
-  //   }
-  // );
+export class AppComponent{
+  constructor(){
 
-  appareils: any[];
-
-  constructor(private appareilService: AppareilService){
-    setTimeout(
-       () => {
-        this.isAuth = true;
-      }, 4000
-    )
-  }
-
-
-  ngOnInit(){
-    this.appareils = this.appareilService.appareils;
-  }
-
-
-  onAllumer(){
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre(){
-    this.appareilService.switchOffAll();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,32 @@ import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
 import { AppareilComponent } from './appareil/appareil.component';
 import { AppareilService } from './services/appareil.service';
+import { AuthComponent } from './auth/auth.component';
+import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+const appRoute: Routes = [
+  {path: 'appareils', component: AppareilViewComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: '', component: AppareilViewComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MonPremierComponent,
-    AppareilComponent
+    AppareilComponent,
+    AuthComponent,
+    AppareilViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoute)
   ],
-  providers: [AppareilService],
+  providers: [AppareilService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
